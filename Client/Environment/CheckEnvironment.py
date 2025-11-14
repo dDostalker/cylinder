76c66f63137a71477bd083c5f3b05836c5e21e43
dDostalker <3824970852@qq.com>
2025-11-14 14:49:21 +0800

    add PhysicalMachineInfo Demo , pytest

--- Client/Environment/CheckEnvironment.py
+++ Client/Environment/CheckEnvironment.py
@@ -1,7 +1,9 @@
 import os
 import re
+from dataclasses import dataclass
 from enum import Enum
 
+import psutil
 from loguru import logger
 
 COMANND_DOCKER = "command -v docker"
@@ -12,6 +14,10 @@ logger.add(LOGFILE, level="INFO", rotation="1MB")
 
 
 class EnvironmentState(Enum):
+    """
+    Check command exist flag
+    """
+
     Uninstall = -1
     Unknow = 0
     Install = 1
@@ -19,8 +25,23 @@ class EnvironmentState(Enum):
     RightDocker = 4
 
 
+@dataclass
+class PhysicalMachineInfo:
+    cpu_count: int | None
+    memory_total: int | None
+    memory_used: int | None
+    os: str | None
+    # todo
+
+
 def CheckDockerExist() -> str | None:
-    logger.info("check Docker Install State")
+    """
+    Check if Docker is installed and return its path if found.
+    Returns:
+        str | None: The path to the Docker executable if found, otherwise None.
+    """
+    logger.info("Start Check Docker Install State")
+
     docker_stauts = EnvironmentState.Unknow.value
     check_docker_pipen = os.popen(COMANND_DOCKER)
     docker_path = check_docker_pipen.read()
@@ -53,5 +74,39 @@ def CheckDockerExist() -> str | None:
         return None
 
 
-if __name__ == "__main__":
+def GetPhysicalMachineInfo() -> PhysicalMachineInfo | None:
+    """
+    Get  information from physical machine
+    Returns:
+        PhysicalMachineInfo | None: The physical machine information if get success, otherwise None.
+    """
+    logger.info("Start check system info")
+
+    try:
+        cpu_count = psutil.cpu_count()
+        logger.info(f"Get cpu count {cpu_count}")
+        memory_info = psutil.virtual_memory()
+        logger.info(f"Get memory info {memory_info}")
+        os = None  # todo
+        logger.info(f"Get os info {os}")
+        return PhysicalMachineInfo(cpu_count, memory_info.total, memory_info.used, os)
+    except Exception as e:
+        logger.error(f"Get physical machine info error: {e}")
+        return None
+
+
+def test_DockerExist():
     CheckDockerExist()
+
+
+def test_physicalMachineInfo():
+    phy_machine_info = GetPhysicalMachineInfo()
+    assert phy_machine_info is not None, "Get physical machine info failed"
+    assert phy_machine_info.cpu_count is not None, "CPU count should be greater than 0"
+    assert phy_machine_info.memory_total is not None, (
+        "Memory total should be greater than 0"
+    )
+    assert phy_machine_info.memory_used is not None, (
+        "Memory used should be greater than 0"
+    )
+    # assert phy_machine_info.os is not None, "OS should not be None"
